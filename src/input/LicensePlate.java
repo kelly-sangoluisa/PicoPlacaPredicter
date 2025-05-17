517f648f2873a5aface4ced679b9900745470295
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-05-16 19:04:34 -0500

    feat: implement LicensePlate utility for plate format validation and last digit extraction

--- src/input/LicensePlate.java
+++ src/input/LicensePlate.java
@@ -1,5 +1,20 @@
 package input;
 
 public class LicensePlate {
-    
+    private String fullPlate;
+
+     public LicensePlate(String fullPlate) {
+        this.fullPlate = fullPlate;
+    }
+
+    public boolean validatePlate() {
+        //placa de ecuador: 3 letras y 3 o 4 digitos
+        return fullPlate.matches("[A-Za-z]{3}-?\\d{3,4}");
+    }
+
+    public int getLastDigit(String fullPlate) {
+        char lastDigiString = fullPlate.charAt(fullPlate.length() - 1);
+        int lastDigit = Character.getNumericValue(lastDigiString);
+        return lastDigit;
+    }
 }
