e20e2be5141020492c047830484e360bb2ac285e
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-05-16 19:11:50 -0500

    feat: add Predictor service to check Pico y Placa restrictions based on date, time, and license plate

--- src/business/Predictor.java
+++ src/business/Predictor.java
@@ -1,5 +1,47 @@
 package business;
 
+import input.DateInput;
+import input.LicensePlate;
+import input.TimeInput;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Predictor {
-    
+    private DateInput date;
+    private TimeInput time;
+    private LicensePlate plate;
+
+    private final RuleSet ruleSet;
+
+    public Predictor(RuleSet ruleSet) {
+        this.ruleSet = ruleSet;
+    }
+
+    public void setDate(DateInput date) {
+        this.date = date;
+    }
+
+    public void setTime(TimeInput time) {
+        this.time = time;
+    }
+
+    public void setPlate(LicensePlate plate) {
+        this.plate = plate;
+    }
+
+    public boolean predict(String rawDate, String rawTime, String fullPlate) {
+        date = new DateInput(rawDate);
+        setDate(date);
+        time = new TimeInput(rawTime);
+        setTime(time);
+        plate = new LicensePlate(fullPlate);
+        setPlate(plate);
+        LocalDate parsedDate = date.parseDate(rawDate);
+        LocalTime parsedTime = time.parseTime(rawTime);
+        DayOfWeek dayOfTheWeek = parsedDate.getDayOfWeek();
+        int lastDigit = plate.getLastDigit(fullPlate);
+        return ruleSet.hasRestriction(dayOfTheWeek, lastDigit, parsedTime);
+    }
 }
