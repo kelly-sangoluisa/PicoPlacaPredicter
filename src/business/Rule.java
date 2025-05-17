2f91c25c4d90ef5734788a3c6f34bb0684b34197
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-05-16 19:08:17 -0500

    feat: implement Rule class to model Pico y Placa regulations

--- src/business/Rule.java
+++ src/business/Rule.java
@@ -1,5 +1,34 @@
 package business;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.List;
+
 public class Rule {
-    
+    private List<DayOfWeek> daysOfWeek;
+    private final List<Integer> restrictedDigits;
+    private final List<Schedule> schedules;
+
+    public Rule(List<DayOfWeek> daysOfWeek, List<Integer> restrictedDigits, List<Schedule> schedules) {
+        this.daysOfWeek = daysOfWeek;
+        this.restrictedDigits = restrictedDigits;
+        this.schedules = schedules;
+    }
+
+    public List<DayOfWeek> getDaysOfWeek() {
+        return daysOfWeek;
+    }
+
+    public List<Integer> getRestrictedDigits() {
+        return restrictedDigits;
+    }
+
+    public boolean isRestricted(DayOfWeek dayOfWeek, int digit, LocalTime time) {
+       for (Schedule schedule : schedules) {
+            if (daysOfWeek.contains(dayOfWeek) && restrictedDigits.contains(digit) && schedule.contains(time)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
