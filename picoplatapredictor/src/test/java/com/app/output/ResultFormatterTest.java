09bdfb9202f6d4c76b901f084b2e77ce092c54f3
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-05-17 03:25:18 -0500

    fix: correct test cases for ResultFormatter to match expected output

--- picoplatapredictor/src/test/java/com/app/output/ResultFormatterTest.java
+++ picoplatapredictor/src/test/java/com/app/output/ResultFormatterTest.java
@@ -7,13 +7,13 @@ class ResultFormatterTest {
 
     @Test
     void testFormatResultCanDrive() {
-        String result = ResultFormatter.formatResult(true);
+        String result = ResultFormatter.formatResult(false);
         assertEquals("You are allowed to drive at this time.", result);
     }
 
     @Test
     void testFormatResultCannotDrive() {
-        String result = ResultFormatter.formatResult(false);
+         String result = ResultFormatter.formatResult(true);
         assertEquals("You are NOT allowed to drive due to 'Pico y Placa' regulations.", result);
     }
 }
