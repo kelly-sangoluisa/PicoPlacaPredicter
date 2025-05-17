0d6ae1fa5d4f2c3ac4c4af050b21f1743e6bdb73
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-05-16 19:17:33 -0500

    feat: add ResultFormatter to generate user-friendly output

--- src/output/ResultFormatter.java
+++ src/output/ResultFormatter.java
@@ -1,5 +1,9 @@
-package output;
 
 public class ResultFormatter {
     
+    public static String formatResult(boolean canDrive) {
+        return canDrive 
+            ? "Puede circular normalmente." 
+            : "No puede circular debido a restricciones de pico y placa.";
+    }
 }
