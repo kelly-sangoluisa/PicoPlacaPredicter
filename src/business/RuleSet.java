a5dc762faa73f4546ed9de286f5e38e8a4af3ab3
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-05-16 19:09:32 -0500

    feat: add RuleSet container for handling multiple restriction rules

--- src/business/RuleSet.java
+++ src/business/RuleSet.java
@@ -1,5 +1,34 @@
 package business;
 
+import java.time.LocalTime;
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class RuleSet {
-    
+    private final List<Rule> rules;
+
+    public RuleSet() {
+        this.rules = new ArrayList<>(6);
+    }
+
+    public void addRule(Rule rule) {
+        rules.add(rule);
+    }
+
+    public boolean hasRules() {
+        return !rules.isEmpty();
+    }
+
+    public boolean hasRestriction(DayOfWeek dayOfWeek, int digit, LocalTime time) {
+        if (!hasRules()) return false;
+
+        for (Rule rule : rules) {
+            if (rule.isRestricted(dayOfWeek, digit, time)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
