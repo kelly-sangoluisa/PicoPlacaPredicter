40c4a82d8a1c14adc20843d473f79449a1f23f38
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-05-16 19:07:00 -0500

    feat: add Schedule class to represent morning and afternoon Pico y Placa time slots

--- src/business/Schedule.java
+++ src/business/Schedule.java
@@ -1,5 +1,17 @@
 package business;
 
+import java.time.LocalTime;
+
 public class Schedule {
-    
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public Schedule(LocalTime startTime, LocalTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public boolean contains(LocalTime time) {
+        return !time.isBefore(startTime) && !time.isAfter(endTime);
+    }
 }
