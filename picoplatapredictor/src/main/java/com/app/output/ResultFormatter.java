ff391263c0e7c84466a306e38204b19feff4bcc5
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-05-17 03:05:41 -0500

    fix: correct order of drive permission messages in formatResult method

--- picoplatapredictor/src/main/java/com/app/output/ResultFormatter.java
+++ picoplatapredictor/src/main/java/com/app/output/ResultFormatter.java
@@ -4,7 +4,7 @@ public class ResultFormatter {
     
     public static String formatResult(boolean canDrive) {
         return canDrive 
-            ? "You are allowed to drive at this time." 
-            : "You are NOT allowed to drive due to 'Pico y Placa' regulations.";
+            ? "You are NOT allowed to drive due to 'Pico y Placa' regulations." 
+            : "You are allowed to drive at this time." ;
     }
 }
