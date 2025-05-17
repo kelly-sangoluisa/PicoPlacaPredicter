09ba50bab2e237cbc3abef45aca51c099916ff31
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-05-16 19:03:01 -0500

    feat: add TimeInput class to parse time strings

--- src/input/TimeInput.java
+++ src/input/TimeInput.java
@@ -1,5 +1,17 @@
 package input;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class TimeInput {
-    
+    private String rawTime;
+
+    public TimeInput(String rawTime) {
+        this.rawTime = rawTime;
+    }
+
+    public LocalTime parseTime(String rawTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return LocalTime.parse(rawTime, formatter);
+    }
 }
